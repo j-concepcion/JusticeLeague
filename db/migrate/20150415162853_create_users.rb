41a29be4d687d9195d2d906a6b3777d04125606f
Janice Concepcion <concepcionja@gmail.com>
2015-04-18 12:18:27 +0800

    Fixed DEVISE (final na talaga... sana)

--- db/migrate/20150415162853_create_users.rb
+++ db/migrate/20150415162853_create_users.rb
@@ -1,9 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users, id: :uuid, default: "uuid_generate_v1()"  do |t|
-      t.string :username
-      t.string :password
-
+      t.string :national_id_number
+	  t.string :first_name
+	  t.string :middle_name
+	  t.string :last_name
+	  t.string :password
+	  
       t.timestamps
     end
   end
--- db/migrate/20150415175005_add_details_to_users.rb
+++ db/migrate/20150415175005_add_details_to_users.rb
@@ -1,9 +1,5 @@
 class AddDetailsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :national_id_number, :string
-    add_column :users, :first_name, :string
-    add_column :users, :middle_name, :string
-    add_column :users, :last_name, :string
     add_column :users, :gender, :string
     add_column :users, :street_adderss, :string
     add_column :users, :date_of_birth, :date
